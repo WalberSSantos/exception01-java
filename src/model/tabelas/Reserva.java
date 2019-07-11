2ad181d729423de3bb6fb662bb64feff769c1af2
Walber Santos <walber_santos@hotmail.com>
2019-07-11 11:45:10 -0300

    Solucao muito ruim

---
+++ src/model/tabelas/Reserva.java
@@ -0,0 +1,65 @@
+package model.tabelas;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	private Integer quartoNumero;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reserva() {
+		
+	}
+
+	public Reserva(Integer quartoNumero, Date checkIn, Date checkOut) {
+		this.quartoNumero = quartoNumero;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getQuartoNumero() {
+		return quartoNumero;
+	}
+
+	public void setQuartoNumero(Integer quartoNumero) {
+		this.quartoNumero = quartoNumero;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long diarias() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.DAYS);
+	}
+	
+	public void atualizaDatas(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Quarto #: "
+				+ quartoNumero
+				+ ", Check-in: "
+				+ sdf.format(checkIn)
+				+ ", Check-Out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ diarias();
+	}
+	
+	
+	
+
+}
